17ce1a868b4e9a258c290c457430d85d0b501e57
mahbubur-rahman1 <you@example.com>
2023-06-11 00:53:24 +0600

    vercel

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
 
 
+const jwtVerify = (req, res, next)=>{
+    const authorization = req.headers.authorization
+    if(!authorization){
+      return res.status(401).send({error: true, message: 'unathorization access'})
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded)=>{
+      if(err){
+        return res.status(401).send({error: true, message: 'unathorization access'})
+      }
+      req.decoded = decoded;
+      next()
+    })
+  }
 
 
 
@@ -27,14 +44,39 @@ async function run() {
     try {
         const instractorColection = client.db("musicSchool").collection(" instructors")
         const classesColection = client.db("musicSchool").collection("classes")
+        const enrollCollection = client.db("musicSchool").collection("allEnroll")
+
+        app.post('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, {expiresIn: '1hr' })
+          res.send({token})
+          })
 
 
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect()
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+          app.get('/enroll', jwtVerify, async(req, res)=>{
+            const email = req.query.email;
+            // console.log(email)
+            if(!email){
+             return res.send([]);
+            }
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail){
+              return res.status(403).send({error: True, message: 'porviden access'})
+            }
+          
+            const query = {email: email};
+            // console.log(query)
+            const result = await enrollCollection.find(query).toArray();
+            res.send(result)
+          })
 
+
+
+          app.post('/all-enroll', async(req, res)=>{
+            const enroll = req.body;
+            const result = await enrollCollection.insertOne(enroll);
+            res.send(result)
+          })
 
 
         app.get('/instructors', async (req, res) => {
@@ -49,6 +91,16 @@ async function run() {
             const result = await classesColection.find().toArray()
             res.send(result)
         })
+
+
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect()
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
 
 
 
