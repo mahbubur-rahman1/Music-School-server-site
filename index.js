ab05bd000f26620fb377fdb8e528580ebb36c426
mahbubur-rahman1 <you@example.com>
2023-06-12 02:28:46 +0600

    delte user addmin

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_KEY)
 
 app.use(cors());
 app.use(express.json());
 
 
-const jwtVerify = (req, res, next)=>{
+const jwtVerify = (req, res, next) => {
     const authorization = req.headers.authorization
-    if(!authorization){
-      return res.status(401).send({error: true, message: 'unathorization access'})
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unathorization access' })
     }
     // bearer token
     const token = authorization.split(' ')[1];
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded)=>{
-      if(err){
-        return res.status(401).send({error: true, message: 'unathorization access'})
-      }
-      req.decoded = decoded;
-      next()
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unathorization access' })
+        }
+        req.decoded = decoded;
+        next()
     })
-  }
+}
 
 
 
@@ -42,41 +43,90 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        const usersCollection = client.db("musicSchool").collection("users")
         const instractorColection = client.db("musicSchool").collection(" instructors")
         const classesColection = client.db("musicSchool").collection("classes")
         const enrollCollection = client.db("musicSchool").collection("allEnroll")
+        const paymentCollection = client.db("musicSchool").collection("payment")
 
-        app.post('/jwt', (req, res)=>{
+
+        app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, {expiresIn: '1hr' })
-          res.send({token})
-          })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, { expiresIn: '1hr' })
+            res.send({ token })
+        })
 
 
-          app.get('/enroll', jwtVerify, async(req, res)=>{
+        app.get('/enroll', jwtVerify, async (req, res) => {
             const email = req.query.email;
             // console.log(email)
-            if(!email){
-             return res.send([]);
+            if (!email) {
+                return res.send([]);
             }
             const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
-              return res.status(403).send({error: True, message: 'porviden access'})
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: True, message: 'porviden access' })
             }
-          
-            const query = {email: email};
+
+            const query = { email: email };
             // console.log(query)
             const result = await enrollCollection.find(query).toArray();
             res.send(result)
+
+
+        })
+
+
+
+
+
+        app.patch('/users/admin/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const updateUser = {
+              $set: {
+                role: 'admin'
+              },
+            }
+            const result = await usersCollection.updateOne(query, updateUser)
+            res.send(result)
+          })
+
+          app.patch('/users/instructor/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const updateUser = {
+              $set: {
+                role: 'instructor' 
+              },
+            }
+            const result = await usersCollection.updateOne(query, updateUser)
+            res.send(result)
+          })
+
+
+        app.delete('/user-delete/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
           })
 
 
 
-          app.post('/all-enroll', async(req, res)=>{
+
+
+
+
+
+
+
+
+        app.post('/all-enroll', async (req, res) => {
             const enroll = req.body;
             const result = await enrollCollection.insertOne(enroll);
             res.send(result)
-          })
+        })
 
 
         app.get('/instructors', async (req, res) => {
@@ -87,13 +137,13 @@ async function run() {
 
 
 
-        app.delete('/enroll/:id', async(req, res)=>{
+        app.delete('/enroll/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await enrollCollection.deleteOne(query)
             res.send(result)
-          })
-          
+        })
+
 
 
 
@@ -102,6 +152,59 @@ async function run() {
             const result = await classesColection.find().toArray()
             res.send(result)
         })
+
+        app.post("/create-payment-intent", jwtVerify, async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            // console.log(price, amount)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            });
+        })
+
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const insertResult = await paymentCollection.insertOne(payment)
+
+            const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+            const deleteResult = await enrollCollection.deleteMany(query)
+
+            res.send({ result: insertResult, deleteResult });
+        })
+
+
+
+        app.get('/my-enroll-class', jwtVerify, async (req, res) => {
+            const email = req.query.email;
+            // console.log(email)
+            if (!email) {
+                return res.send([]);
+            }
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: True, message: 'porviden access' })
+            }
+
+            const query = { email: email };
+            // console.log(query)
+            const result = await paymentCollection.find(query).toArray();
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
 
 
         // Connect the client to the server	(optional starting in v4.7)
